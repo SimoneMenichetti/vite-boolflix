03573fbe16217cc62e7854781ef43e8fe7f07faf
simone menichetti <simon3menichettui95@gmail.com>
2024-07-11 16:42:17 +0200

    feat:(add new component for single movie , export and import in MovieDetails and add component )

--- src/components/AppMovieDetailsList.vue
+++ src/components/AppMovieDetailsList.vue
@@ -1,40 +1,18 @@
 <script>
+// importiamo il componente SingleMovie
+import SingleMovieDetail from './SingleMovieDetail.vue';
+
 export default{
-    name:'AppMovieDetailslist'
+    name:'AppMovieDetailslist',
+    components:{
+        SingleMovieDetail,
+    }
 }
 </script>
 
 <template>
      <div class="container">
-        <div class="movie-details">
-            <h3>Movie Title 1</h3>
-            <p><strong>Original Title:</strong> Original Title 1</p>
-            <p><strong>Language:</strong> English</p>
-            <p><strong>Rating:</strong></p>
-            <p><strong>Trame:</strong></p>
-        </div>
-        <div class="movie-details">
-            <h3>Movie Title 2</h3>
-            <p><strong>Original Title:</strong> Original Title 2</p>
-            <p><strong>Language:</strong> Spanish</p>
-            <p><strong>Rating:</strong></p>
-            <p><strong>Trame:</strong></p>
-        </div>
-        <div class="movie-details">
-            <h3>Movie Title 3</h3>
-            <p><strong>Original Title:</strong> Original Title 3</p>
-            <p><strong>Language:</strong> French</p>
-            <p><strong>Rating:</strong></p>
-            <p><strong>Trame:</strong></p>
-        </div>
-        <div class="movie-details">
-            <h3>Movie Title 4</h3>
-            <p><strong>Original Title:</strong> Original Title 4</p>
-            <p><strong>Language:</strong> Italian</p>
-            <p><strong>Rating:</strong></p>
-            <p><strong>Trame:</strong></p>
-        </div>
-       <!-- qui aggiunta card dei film / serie -->
+        <SingleMovieDetail/>
     </div>
 </template>
 
@@ -42,16 +20,4 @@ export default{
 @use '../assets/style/general.scss'as*;
 
 
-.movie-details {
-    box-sizing: border-box;
-    padding: 16px;
-    border: 1px solid #ccc;
-    text-align: center;
-    background-color: #f9f9f9;
-
-    .movie-details h3 {
-        margin-top: 0;
-    }
-
-}
 </style>
---
+++ src/components/SingleMovieDetail.vue
@@ -0,0 +1,55 @@
+<script>
+
+export default{
+    name:"SingleMovieDetail",
+}
+</script>
+
+<template>
+     <div class="movie-details">
+            <h3>Movie Title 1</h3>
+            <p><strong>Original Title:</strong> Original Title 1</p>
+            <p><strong>Language:</strong> English</p>
+            <p><strong>Rating:</strong></p>
+            <p><strong>Trame:</strong></p>
+        </div>
+        <div class="movie-details">
+            <h3>Movie Title 2</h3>
+            <p><strong>Original Title:</strong> Original Title 2</p>
+            <p><strong>Language:</strong> Spanish</p>
+            <p><strong>Rating:</strong></p>
+            <p><strong>Trame:</strong></p>
+        </div>
+        <div class="movie-details">
+            <h3>Movie Title 3</h3>
+            <p><strong>Original Title:</strong> Original Title 3</p>
+            <p><strong>Language:</strong> French</p>
+            <p><strong>Rating:</strong></p>
+            <p><strong>Trame:</strong></p>
+        </div>
+        <div class="movie-details">
+            <h3>Movie Title 4</h3>
+            <p><strong>Original Title:</strong> Original Title 4</p>
+            <p><strong>Language:</strong> Italian</p>
+            <p><strong>Rating:</strong></p>
+            <p><strong>Trame:</strong></p>
+        </div>
+</template>
+
+<style scoped lang="scss">
+@use '../assets/style/general.scss' as*;
+
+
+.movie-details {
+    box-sizing: border-box;
+    padding: 16px;
+    border: 1px solid #ccc;
+    text-align: center;
+    background-color: #f9f9f9;
+
+    .movie-details h3 {
+        margin-top: 0;
+    }
+
+}
+</style>
