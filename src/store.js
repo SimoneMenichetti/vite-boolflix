e3024722bef639a0215da1b20fb0b283dbdd9d03
simone menichetti <simon3menichettui95@gmail.com>
2024-07-11 13:07:36 +0200

    fix:error( api url)

--- src/store.js
+++ src/store.js
@@ -3,5 +3,6 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     apiURL: `https://api.themoviedb.org/3/movie/550?api_key=2151d1163db8f79c65ffd8f6a53575be&query=`,
+    searchQuery: '',
 });
 
