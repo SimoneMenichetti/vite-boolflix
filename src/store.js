561d937c9575182658f064f57307ed14c10d58d0
simone menichetti <simon3menichettui95@gmail.com>
2024-07-11 13:06:13 +0200

    feat:store.js( add api url)

--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    apiURL:
-
+    apiURL: `https://api.themoviedb.org/3/movie/550?api_key=2151d1163db8f79c65ffd8f6a53575be&query=`,
 });
 
